7f7cc701193c5fd0ed774f8b7b37e1018f2963fe
Chris Kang <chris.v.kang@gmail.com>
2026-05-10 20:58:08 -0500

    fix: wrap app in GestureHandlerRootView for Android gesture support

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,16 @@
 import { Stack } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { LibraryProvider } from '../src/context/LibraryContext';
 
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <LibraryProvider>
-        <Stack screenOptions={{ headerShown: false }} />
-      </LibraryProvider>
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <LibraryProvider>
+          <Stack screenOptions={{ headerShown: false }} />
+        </LibraryProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
